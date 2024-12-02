8acdba115dc385d2a4d3be62f1d1314f19ad2ae1
Abdulkadir <kattaskadir0@gmail.com>
2024-12-02 15:07:23 +0300

    cors configure

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import routes from "./routers/routes";
 
 const app = express();
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(compression());
 app.use(cookieParser());
